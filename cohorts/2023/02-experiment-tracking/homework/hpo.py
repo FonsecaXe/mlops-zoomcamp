eafb2f49c0308ea5299dbff2605666235781bc4c
Emanuel Fonseca <emanuelcf@gmail.com>
2023-07-31 19:37:45 +0000

    homework_3

--- cohorts/2023/02-experiment-tracking/homework/hpo.py
+++ cohorts/2023/02-experiment-tracking/homework/hpo.py
@@ -29,11 +29,13 @@ def load_pickle(filename):
     help="The number of parameter evaluations for the optimizer to explore"
 )
 def run_optimization(data_path: str, num_trials: int):
-
+    mlflow.sklearn.autolog(disable=True)
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(trial):
+        
+
         params = {
             'n_estimators': trial.suggest_int('n_estimators', 10, 50, 1),
             'max_depth': trial.suggest_int('max_depth', 1, 20, 1),
@@ -42,11 +44,13 @@ def run_optimization(data_path: str, num_trials: int):
             'random_state': 42,
             'n_jobs': -1
         }
-
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        with mlflow.start_run():
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
 
         return rmse
 
